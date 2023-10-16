e7991072143e1da87428367f3ce65bffb6d97d37
C4lopsitta <simone@atomtech.cc>
2023-10-16 14:18:14 +0200

    Backported output file thingy

--- Main.java
+++ Main.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 public class Main{
   public static void main(String[] args){
     if(args.length != 2){
-      System.out.println("USAGE: java Main inputFile.extension outputFile");
+      System.out.println("USAGE: java Main inputFile outputFile");
     }
 
     String fileName = args[0];
     String outFileName = args[1];
     String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    String outFleExtension = outFileName.substring(outFileName.lastIndexOf('.') + 1);
 
     //try catch statement to catch possible exception in file opening
     try {
@@ -30,7 +31,7 @@ public class Main{
         f.setEurkg((int)(f.getEurkg() * 1.1));
         System.out.println(f);
       }
-      fruttaFile.writeFile((outFileName + "." + fileExtension) , frutti);
+      fruttaFile.writeFile(outFileName, frutti);
 
     } catch (IOException e) {
       e.printStackTrace();
