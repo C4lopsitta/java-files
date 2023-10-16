305f1afeb4df862e9afb17123014297b1a0054c9
C4lopsitta <simone@atomtech.cc>
2023-10-16 14:36:17 +0200

    Post merge

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -cp *.java "./:./gson-2.10.1.jar:./jakarta.activation-api-2.0.0.jar:./jakarta.xml.bind-api-3.0.0.jar:./jaxb-core-3.0.0.jar:./jaxb-impl-3.0.0.jar:./jaxb-runtime-3.0.0.jar"
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp "./:./gson-2.10.1.jar:./jakarta.activation-api-2.0.0.jar:./jakarta.xml.bind-api-3.0.0.jar:./jaxb-core-3.0.0.jar:./jaxb-impl-3.0.0.jar:./jaxb-runtime-3.0.0.jar" $1
