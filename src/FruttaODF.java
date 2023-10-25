abc1d0f709d837cf3ec008f928f8a748a1b32a47
C4lopsitta <simone@atomtech.cc>
2023-10-25 12:04:53 +0200

    Implementing ODF files

---
+++ src/FruttaODF.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import jakarta.xml.bind.JAXBException;
+
+public class FruttaODF implements IFruttoFile {
+  public ArrayList<Frutta> readFile(String filename) throws IOException, JAXBException {
+
+  }
+
+  public void writeFile(String filename, ArrayList<Frutta> frutti) throws IOException, JAXBException {
+
+  }
+}
+
